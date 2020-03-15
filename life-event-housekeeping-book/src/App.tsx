f6b964f9a5b2d432fe7eefb26eef016adb1acd5d
nainaistar <nainaistar@gmail.com>
2020-03-15 22:08:26 +0900

    [Add] const化

--- life-event-housekeeping-book/src/App.tsx
+++ life-event-housekeeping-book/src/App.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App: React.FC = () =>{
   return (
     <div className="App">
       <header className="App-header">
@@ -17,10 +17,11 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
+          And Gorilla
         </a>
       </header>
     </div>
-  );
-}
+  )
+};
 
 export default App;
